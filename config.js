aebb367673ce893726c577b41377fa8f083765da
Hazem2030Hazem <hazemalheandawy2030@gmail.com>
2026-08-07 16:53:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,5 @@
 // Project URL و anon key من: Project Settings → API
 const HAZEM_SUPABASE_URL = 'https://nqporduxtzrojnclkdbu.supabase.co';
 const HAZEM_SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xcG9yZHV4dHpyb2puY2xrZGJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMTE4ODEsImV4cCI6MjEwMTU4Nzg4MX0.Quel6oZnVCGf41qP85WQKwSRJO-psjyFssd6Mavp_uE';
+
+const HAZEM_OWNER_EMAIL = 'Hazemalheandawy2030@gmail.com';
